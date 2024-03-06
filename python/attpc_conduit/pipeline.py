e6ead5c67318e9f2f798b2b34f520c9f5188cc0e
gwm17 <gordonmccann215@gmail.com>
2024-03-06 10:42:49 -0500

    Fix some pipeline jank that now seems to really boost the performance the viewer.

--- python/attpc_conduit/pipeline.py
+++ python/attpc_conduit/pipeline.py
@@ -23,15 +23,15 @@ def init_detector_bounds() -> None:
     # log the pad plane bounds
     plane = generate_circle_for_render(0.0, 0.0, 300.0)
     rr.log(
-        "geometry/2d/plane",
+        "bounds/plane",
         rr.LineStrips2D(plane),
         timeless=True,
     )
     # log the coordinate orientation for 3D
-    rr.log("geometry/3d/", rr.ViewCoordinates.RIGHT_HAND_X_UP, timeless=True)
+    rr.log("/", rr.ViewCoordinates.RIGHT_HAND_X_UP, timeless=True)
     # log the detector box
     rr.log(
-        "geometry/3d/detector_box",
+        "bounds/box",
         rr.Boxes3D(half_sizes=[300.0, 300.0, 500.0], centers=[0.0, 0.0, 500.0]),
         timeless=True,
     )
@@ -81,8 +81,9 @@ def run_pipeline(
         The current conduit configuration
     """
     rr.set_time_sequence("event_time", event_id)
-    rr.log("event", rr.Clear(recursive=True))
-    rr.log("event", rr.Clear(recursive=True))
+    rr.log("raw", rr.Clear(recursive=True))
+    rr.log("clusters", rr.Clear(recursive=True))
+    rr.log("circles", rr.Clear(recursive=True))
 
     pc = phase_pointcloud(event_id, event_matrix, pad_map, config.get, config.detector)
     if len(pc.cloud) < 2:
@@ -91,13 +92,13 @@ def run_pipeline(
     colors = generate_point_colors(pc.cloud[:, 3])
     radii = np.full(len(pc.cloud), RADIUS)
     rr.log(
-        f"event/3d/point_cloud",
+        f"raw/cloud",
         rr.Points3D(
             pc.cloud[:, :3], radii=radii, colors=colors, labels=pc.get_hardware_labels()
         ),
     )
     rr.log(
-        f"event/2d/point_cloud",
+        f"raw/plane",
         rr.Points2D(
             pc.cloud[:, :2],
             radii=radii,
@@ -117,12 +118,12 @@ def run_pipeline(
         )
         radii = np.full(len(pc.cloud), RADIUS)
         rr.log(
-            "event/3d/clusters",
+            "clusters/cloud",
             rr.Points3D(pc.cloud[:, :3], radii=radii, class_ids=labels),
             context,
         )
         rr.log(
-            "event/2d/clusters",
+            "clusters/plane",
             rr.Points2D(pc.cloud[:, :2], radii=radii, class_ids=labels),
             context,
         )
@@ -144,13 +145,13 @@ def run_pipeline(
             )
             circle_points[:, 2] = est.vertex[2]
             rr.log(
-                f"event/3d/circles/",
+                f"circles/cloud",
                 rr.LineStrips3D(circle_points, radii=RADIUS, class_ids=idx),
                 circles_context,
             )
 
             rr.log(
-                f"event/2d/circles",
+                f"circles/plane",
                 rr.LineStrips2D(circle_points[:, :2], radii=RADIUS, class_ids=idx),
                 circles_context,
             )
