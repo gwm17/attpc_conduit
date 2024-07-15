d823eeb7329564b688d54875e529b8b01d99360d
gwm17 <gordonmccann215@gmail.com>
2024-07-14 20:37:07 -0400

    Fix insane memory usage by 2D histos by establishing them as static. Update blueprint for more customization for display of histograms

--- python/attpc_conduit/core/blueprint.py
+++ python/attpc_conduit/core/blueprint.py
@@ -1,4 +1,5 @@
 import rerun.blueprint as bpt
+import rerun as rr
 
 
 def generate_default_blueprint() -> bpt.Blueprint:
@@ -12,7 +13,24 @@ def generate_default_blueprint() -> bpt.Blueprint:
                 bpt.BarChartView(
                     name="1D-Histograms", contents="$origin/histograms/**"
                 ),
-                bpt.TensorView(name="2D-Histograms", contents="$origin/histograms/**"),
+                bpt.TensorView(
+                    name="Kinematics",
+                    contents="$origin/histograms/kinematics",
+                    view_fit="fill",
+                    slice_selection=bpt.TensorSliceSelection(
+                        width=rr.TensorDimensionSelection(dimension=1, invert=False),
+                        height=rr.TensorDimensionSelection(dimension=0, invert=True),
+                    ),
+                ),
+                bpt.TensorView(
+                    name="Particle ID",
+                    contents="$origin/histograms/particle_id",
+                    view_fit="fill",
+                    slice_selection=bpt.TensorSliceSelection(
+                        width=rr.TensorDimensionSelection(dimension=1, invert=False),
+                        height=rr.TensorDimensionSelection(dimension=0, invert=True),
+                    ),
+                ),
                 bpt.TextLogView(name="Logs"),
             ),
         )
